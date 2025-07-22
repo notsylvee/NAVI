fa2729735619ed71488abec4023caf5ecd29b3f6
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-21 23:55:29 -0400

    updated voicelines

--- src/events/taunt.js
+++ src/events/taunt.js
@@ -1,21 +1,20 @@
-const { PermissionFlagsBits } = require("discord.js");
+const fs = require("fs/promises");
 
 module.exports = {
-    name: "messageCreate",
-    async execute(message) {
+  name: "messageCreate",
+  async execute(message) {
+    if (message.author.bot || !message.guild) return;
 
-        if (message.author.bot || !message.guild) return;
-        if (!message.guild.members.me.permissionsIn(message.channel.id).has(PermissionFlagsBits.SendMessages)) return;
-        const chance = Math.random() * 750;
-        const taunts = [
-          "Since you're unable to follow simple instructions, I have been given permission to terminate you. Goodbye.",
-          "It would seem as if you do not have the cognitive function required to do your tasks. Please stand by for execution."
-        ];
-        const taunt = taunts[Math.floor(Math.random() * taunts.length)];
-        if (chance < 747) {
-            return;
-          } else {
-            message.channel.send(`<:Navi:1395296283333234799> ${taunt}`);
-          };
-    },
+    const tauntsJsonData = await fs.readFile("data/taunts.json", {encoding: "utf8"});
+    const tauntsMap = JSON.parse(tauntsJsonData);
+    const taunts = tauntsMap["taunts"];
+    const taunt = taunts[Math.floor(Math.random() * taunts.length)];
+
+    const chance = Math.random() * 900;
+    if (chance < 898) {
+      return;
+    } else {
+      message.channel.send(`<:Navi:1395296283333234799> ${taunt.text}`).catch((err) => {return});
+    };
+  },
 };
